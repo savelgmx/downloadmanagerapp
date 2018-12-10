35b7b0099973cf7696b23a5a54f2207f6efdf017
savelgmx <savel@gmx.com>
2018-12-10 16:43:38 +0700

    ImageView показывает скачанное изображение из URL  //TODO проверка УРЛа на прваильность

--- app/src/main/java/fb/fandroid/adv/downloadmanagerapp/MainActivity.java
+++ app/src/main/java/fb/fandroid/adv/downloadmanagerapp/MainActivity.java
@@ -8,6 +8,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -22,6 +24,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.util.ArrayList;
 
 /*
@@ -36,104 +39,12 @@ https://medium.com/@crossphd/android-image-loading-from-a-string-url-6c8290b82c5
 public class MainActivity extends AppCompatActivity {
 
     public static final String URL_REGEX = "^((https?|ftp):\\/\\/|(www|ftp)\\.)?[a-z0-9-]+(\\.[a-z0-9-]+)+([\\/?].*)?$";
-
+    ArrayList<Long> list = new ArrayList<>();
     private DownloadManager downloadManager;
     private long refid;    //отправляется в broadcast receiver
     private Uri Download_Uri; //http://www.yaplakal.com/html/static/top-logo.png
-    ArrayList<Long> list = new ArrayList<>();
-
     private Button mButtonOne;
     private Button mButtonTwo;
-    private EditText mEditText;
-    private ImageView mImageView;
-
-    //-----обработка нажатия на кнопки
-    private View.OnClickListener mOnButtonOneClickListener;
-    private View.OnClickListener mOnButtonTwoClickListener;
-
-    {
-        mOnButtonOneClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                //здесь мы производим загрузку 1 файла через класс DownloadManager
-                list.clear();
-                DownloadManager.Request request = new DownloadManager.Request(Download_Uri);
-                request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE);
-                request.setAllowedOverRoaming(false);
-                request.setTitle("YP Downloading " + "YapLogo" + ".png");
-                request.setDescription("Downloading " + "YapLogo" + ".png");
-                request.setVisibleInDownloadsUi(true);
-                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "/Yaplakal/"  + "/" + "YapLogo" + ".png");
-                refid = downloadManager.enqueue(request);
-                list.add(refid);
-                Log.e("OUT", "" + refid);
-              }
-
-        };
-    }
-
-    {
-        mOnButtonTwoClickListener = new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-/*
-               сначала мы здесь проверяем введеный EditText на ссответвие REGEXP
-               т.е. явлвяется ли  это УРЛом вида https://yaplakal.com
-                Заодно проверим 3 последних символа в ссылке
-*/
-
-            }
-
-        };
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-
-        registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE)); //будем использвать бродкаст ресивер для сигнала об окончании загрузки
-        Download_Uri = Uri.parse("http://www.yaplakal.com/html/static/top-logo.png");
-
-        mButtonOne =findViewById(R.id.buttonOne);
-        mButtonTwo =findViewById(R.id.buttonTwo);
-        mEditText =findViewById(R.id.editText);
-        mImageView=findViewById(R.id.imageView);
-
-        mButtonOne.setOnClickListener(mOnButtonOneClickListener);//иницыализируем обработчик нажатия на кнопки
-        mButtonTwo.setOnClickListener(mOnButtonTwoClickListener);
-        mButtonTwo.setEnabled(false);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            mImageView.setImageDrawable(getDrawable(R.drawable.sun));
-        }
-
-
-        if (!isStoragePermissionGranted()) {
-            //разрешений нет работу прекращаем
-            Toast.makeText(this, "Не удалось получить разрешение на запись во внешнее хранилище ", Toast.LENGTH_SHORT).show();
-            System.exit(1);
-        }
-
-    }
-
-    public boolean isStoragePermissionGranted() {//проверим разрешение на запись во внешнее хранилище  WRITE_EXTERNAL_STORAGE
-        if (Build.VERSION.SDK_INT >= 23) {
-            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    == PackageManager.PERMISSION_GRANTED) {
-                return true;
-            } else {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-                return false;
-            }
-        } else { //На sdk<23 разрешение выдается автоматически при установке программы
-            return true;
-        }
-    }
-
     BroadcastReceiver onComplete = new BroadcastReceiver() {
 
         @Override
@@ -158,12 +69,113 @@ public class MainActivity extends AppCompatActivity {
                 NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
                 notificationManager.notify(455, mBuilder.build());
 
-               mButtonTwo.setEnabled(true); //теперь можно и загрузить картинку
+                mButtonTwo.setEnabled(true); //теперь можно и загрузить картинку
 
             }
 
         }
     };
+
+    private EditText mEditText;
+    private ImageView mImageView;
+    //-----обработка нажатия на кнопки
+    private View.OnClickListener mOnButtonOneClickListener;
+    private View.OnClickListener mOnButtonTwoClickListener;
+
+    {
+        mOnButtonOneClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+/*
+               сначала мы здесь проверяем введеный EditText на ссответвие REGEXP
+               т.е. явлвяется ли  это УРЛом вида https://yaplakal.com
+                Заодно проверим 3 последних символа в ссылке
+               Если ссылка, не оканчивается на .jpeg/.png/.bmp то выходим
+*/
+
+                //здесь мы производим загрузку 1 файла через класс DownloadManager
+                list.clear();
+                DownloadManager.Request request = new DownloadManager.Request(Download_Uri);
+                request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE);
+                request.setAllowedOverRoaming(false);
+                request.setTitle("YP Downloading " + "YapLogo" + ".png");
+                request.setDescription("Downloading " + "YapLogo" + ".png");
+                request.setVisibleInDownloadsUi(true);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "/Yaplakal/"  + "/" + "YapLogo" + ".png");
+                refid = downloadManager.enqueue(request);
+                list.add(refid);
+                Log.e("OUT", "" + refid);
+            }
+
+        };
+    }
+
+    {
+        mOnButtonTwoClickListener = new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                File imgFile = new  File("/sdcard/"+Environment.DIRECTORY_DOWNLOADS+"/Yaplakal/"  + "/" + "YapLogo" + ".png");
+
+                if(imgFile.exists()) {
+
+                    Bitmap downloadImageBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+
+                    mImageView.setImageBitmap(downloadImageBitmap);
+                }
+            }
+
+        };
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+
+        registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE)); //будем использвать бродкаст ресивер для сигнала об окончании загрузки
+        Download_Uri = Uri.parse("http://www.yaplakal.com/html/static/top-logo.png");
+
+
+
+        mButtonOne =findViewById(R.id.buttonOne);
+        mButtonTwo =findViewById(R.id.buttonTwo);
+        mEditText =findViewById(R.id.editText);
+        mImageView=findViewById(R.id.imageView);
+
+        mButtonOne.setOnClickListener(mOnButtonOneClickListener);//иницыализируем обработчик нажатия на кнопки
+        mButtonTwo.setOnClickListener(mOnButtonTwoClickListener);
+        mButtonTwo.setEnabled(false);
+
+/*
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mImageView.setImageDrawable(getDrawable(R.drawable.sun));
+        }
+*/
+
+
+        if (!isStoragePermissionGranted()) {
+            //разрешений нет работу прекращаем
+            Toast.makeText(this, "Не удалось получить разрешение на запись во внешнее хранилище ", Toast.LENGTH_SHORT).show();
+            System.exit(1);
+        }
+
+    }
+
+    public boolean isStoragePermissionGranted() {//проверим разрешение на запись во внешнее хранилище  WRITE_EXTERNAL_STORAGE
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                return true;
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        } else { //На sdk<23 разрешение выдается автоматически при установке программы
+            return true;
+        }
+    }
 
     protected void onDestroy() {
 
